147e59462eb6b958904e9ce619513b2d94a34c27
Ronald McGuire <ronaldmcguire@PSADMINs-MacBook-Air-86.local>
2020-07-03 12:35:57 -0400

    this has a brief discrpition of the story and how things will be laid out

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// rock paper scissors code
+// ==================== rock paper scissors code =============================
 
 let p1 = prompt(" pick player 1: rock, paper or scissors ");
 let p2 = prompt(" pick player 2: rock, paper or scissors ");
@@ -27,5 +27,7 @@ if(choice1 === " scissors " && choice2 === " paper "){
     alert(" player 2 wins ")
 }
 
-
 rowshambow(p1 , p2);
+
+//====================== Story code (butterfly effect) ======================
+// story will be a horror game based. This will mostly focus on a child who is moving to a new city. mother as recieved a new job and the father is able to work anywhere due to his job. wihtin the story, the day by day events will result the outcome of the bigger story. ultimately this story will have an ending regarding the death of the mom and dad in the hands of the child.

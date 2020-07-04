c19368c0c20843409a385bcb99ac7f5886aea9d7
Ronald McGuire <ronaldmcguire@PSADMINs-MacBook-Air-86.local>
2020-07-04 18:16:00 -0400

    this is where i added the button for the beginning of the game.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // ==================== rock paper scissors code =============================
 let rowshambow = (choice1,choice2) => {
+
 if (choice1 === choice2 ){
     alert("it is a tie! ")
 }
@@ -17,27 +18,28 @@ if(choice1 === "scissors" && choice2 === "rock"){
     alert(" player 1 wins ")
 }
 
-if(choice1 === " scissors " && choice2 === " paper "){
+if(choice1 === "scissors" && choice2 === "paper"){
     alert(" player 1 wins ")
-}else if (choice1 === " paper " && choice2 === " scissors "){
+}else if (choice1 === "paper" && choice2 === "scissors"){
     alert(" player 2 wins ")
 }
+
 }
 
 //====================== Story code (butterfly effect) ======================
 // story will be a horror game based. This will mostly focus on a child who is moving to a new city. mother as recieved a new job and the father is able to work anywhere due to his job. wihtin the story, the day by day events will result the outcome of the bigger story. ultimately this story will have an ending with the death of the mom and dad in the hands of the child.
 
-let p1 = prompt(" player one: PB&J or cereal ?");
-let p2 = prompt (" player two: PB&J or cereal ?");
-if( p1 === p2){
-    alert(" I picked up the " + p1 + " and ate it")
-}else if(p1 !== p2) {
+// let p1 = prompt(" player one: PB&J or cereal ?");
+// let p2 = prompt (" player two: PB&J or cereal ?");
+// if( p1 === p2){
+//     alert(" I picked up the " + p1 + " and ate it")
+// }else if(p1 !== p2) {
 
-let p1 = prompt(" pick player 1: rock, paper or scissors ");
-let p2 = prompt(" pick player 2: rock, paper or scissors ");
+// let p1 = prompt(" pick player 1: rock, paper or scissors ");
+// let p2 = prompt(" pick player 2: rock, paper or scissors ");
 
-rowshambow(p1 , p2);
+// rowshambow(p1 , p2);
 
-}
+// }
 
 //============= most of the code that will be laid out throughout the story =========

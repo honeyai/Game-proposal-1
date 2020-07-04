3f230115a7313ac57f2575dfef2cf08528e489e8
Ronald McGuire <ronaldmcguire@PSADMINs-MacBook-Air-86.local>
2020-07-04 12:50:54 -0400

    had to update things. update the js name

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 // ==================== rock paper scissors code =============================
-
 let rowshambow = (choice1,choice2) => {
 if (choice1 === choice2 ){
     alert("it is a tie! ")
 }
-}
+
 if(choice1 === "rock" && choice2 === "paper"){
     alert(" player 2 wins ")
 
@@ -23,8 +22,7 @@ if(choice1 === " scissors " && choice2 === " paper "){
 }else if (choice1 === " paper " && choice2 === " scissors "){
     alert(" player 2 wins ")
 }
-
-rowshambow(p1 , p2);
+}
 
 //====================== Story code (butterfly effect) ======================
 // story will be a horror game based. This will mostly focus on a child who is moving to a new city. mother as recieved a new job and the father is able to work anywhere due to his job. wihtin the story, the day by day events will result the outcome of the bigger story. ultimately this story will have an ending with the death of the mom and dad in the hands of the child.
@@ -32,10 +30,14 @@ rowshambow(p1 , p2);
 let p1 = prompt(" player one: PB&J or cereal ?");
 let p2 = prompt (" player two: PB&J or cereal ?");
 if( p1 === p2){
-    alert(" I picked up the " + p1 + "and ate it")
-}else{
+    alert(" I picked up the " + p1 + " and ate it")
+}else if(p1 !== p2) {
+
 let p1 = prompt(" pick player 1: rock, paper or scissors ");
 let p2 = prompt(" pick player 2: rock, paper or scissors ");
-    rowshambow(p1 , p2);
+
+rowshambow(p1 , p2);
+
 }
+
 //============= most of the code that will be laid out throughout the story =========
